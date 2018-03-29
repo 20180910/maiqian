b9171ff079d6897d9412d2aa6a4baba25f0f4b4f
2380253499@qq.com <2380253499@qq.com>
2018-03-29 11:09:46 +0800

    项目文件

---
+++ app/src/main/java/com/sk/maiqian/module/my/activity/EditNicknameActivity.java
@@ -0,0 +1,33 @@
+package com.sk.maiqian.module.my.activity;
+
+import android.view.View;
+
+import com.sk.maiqian.R;
+import com.sk.maiqian.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+
+public class EditNicknameActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        setAppTitle("编辑昵称");
+        return R.layout.editnickname_act;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/sk/maiqian/module/my/activity/YiJianFanKuiActivity.java
@@ -0,0 +1,34 @@
+package com.sk.maiqian.module.my.activity;
+
+import android.view.View;
+
+import com.sk.maiqian.R;
+import com.sk.maiqian.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+
+public class YiJianFanKuiActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        setAppTitle("意见反馈");
+        setAppRightImg(R.drawable.share);
+        return R.layout.yijianfankui_act;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
