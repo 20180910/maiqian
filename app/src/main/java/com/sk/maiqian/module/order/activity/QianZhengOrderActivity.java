f9261c2ba4be6de14c0a8b4cbc8eb5a83467eeef
2380253499@qq.com <2380253499@qq.com>
2018-03-30 10:44:05 +0800

    项目文件

---
+++ app/src/main/java/com/sk/maiqian/module/order/activity/QianZhengOrderActivity.java
@@ -0,0 +1,34 @@
+package com.sk.maiqian.module.order.activity;
+
+import android.view.View;
+
+import com.sk.maiqian.R;
+import com.sk.maiqian.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/3/30.
+ */
+
+public class QianZhengOrderActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        setAppTitle("签证代办详情");
+        setAppRightImg(R.drawable.share);
+        return R.layout.qianzhengorder_act;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
--- app/src/main/java/com/sk/maiqian/module/order/fragment/OrderListFragment.java
+++ app/src/main/java/com/sk/maiqian/module/order/fragment/OrderListFragment.java
@@ -1,15 +1,25 @@
 package com.sk.maiqian.module.order.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.github.androidtools.PhoneUtils;
+import com.github.baseclass.adapter.MyLoadMoreAdapter;
+import com.github.baseclass.adapter.MyRecyclerViewHolder;
 import com.sk.maiqian.R;
 import com.sk.maiqian.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2018/3/30.
  */
 
 public class OrderListFragment extends BaseFragment {
+    @BindView(R.id.rv_order_list)
+    RecyclerView rv_order_list;
+    MyLoadMoreAdapter adapter;
     @Override
     protected int getContentView() {
         return R.layout.orderlist_frag;
@@ -17,6 +27,17 @@ public class OrderListFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        adapter=new MyLoadMoreAdapter<String>(mContext,R.layout.orderlist_item,pageSize) {
+            @Override
+            public void bindData(MyRecyclerViewHolder holder, int position, String bean) {
+
+            }
+        };
+        adapter.setOnLoadMoreListener(this);
+        rv_order_list.setLayoutManager(new LinearLayoutManager(mContext));
+        rv_order_list.addItemDecoration(getItemDivider(PhoneUtils.dip2px(mContext,5)));
+        rv_order_list.setAdapter(adapter);
+
 
     }
 
